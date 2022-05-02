3c5e0288bcde5c38d3961f69a9e23125b17b0a22
iLynette <lynetteacholah@icloud.com>
2022-05-02 20:41:15 +0300

    Neutered animals that have <3 escape attempts

--- queries.sql
+++ queries.sql
@@ -5,3 +5,6 @@ SELECT * FROM animals WHERE name LIKE '%mon';
 
 -- List the name of all animals born between 2016 and 2019.
 SELECT * from animals WHERE `date_of_birth` between '2016-01-01'  and '2019-01-01';
+
+-- List the name of all animals that are neutered and have less than 3 escape attempts.
+SELECT * FROM animals WHERE `neutered` = true AND escape_attempts < 3;
