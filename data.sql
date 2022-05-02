1cfd82316406c0d9ec0dfb7f54876abd168821b2
iLynette <lynetteacholah@icloud.com>
2022-05-02 19:22:09 +0300

    Insert Gabumon into table

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', '2020-02-03', 10.23, TRUE, 0);
-
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', '2018-15-11', 8, TRUE, 2);
