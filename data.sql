ff237747ada168745f98c3fa63d4aafe38574b98
iLynette <lynetteacholah@icloud.com>
2022-05-04 15:19:52 +0300

    Modify inserted animals

--- data.sql
+++ data.sql
@@ -4,6 +4,8 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', '2018-15-11', 8, TRUE, 2);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Pikachu', '2021-01-07', 15.04, FALSE, 1);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', '2017-05-12', 11, TRUE, 5);
+
+-- Day two project
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Charmander', '2020-02-03', -11, FALSE, 0);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Plantmon', '2020-11-15', -5.7, TRUE, 2);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Squirtle', '1993-04-02', -12.13, FALSE, 3);
@@ -23,4 +25,16 @@ INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
 -- Insert the following data into the species table:
 INSERT INTO species (name) VALUES ('Pokemon');
 INSERT INTO species (name) VALUES ('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value:
+-- If the name ends in "mon" it will be Digimon
+-- All other animals are Pokemon
+BEGIN;
+UPDATE animals
+SET species_id = CASE
+	WHEN NAME LIKE '%mon'
+    THEN (SELECT id FROM species WHERE name = 'Digimon')
+	ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+ END;
+
 
